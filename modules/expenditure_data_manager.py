8b9efaa7d8628f99e8e33d2484af8dddca4a9437
Rurality <k22056kk@aitech.ac.jp>
2024-01-24 23:23:41 +0900

    カレンダーページの機能強化

--- modules/expenditure_data_manager.py
+++ modules/expenditure_data_manager.py
@@ -6,6 +6,103 @@ from datetime import datetime
 file_path = './modules/balance_of_account.json'
 file_path_obj = pathlib.Path(file_path)
 
+def get_days_amount_data(date):
+    """
+    指定月の支出データ（日ごと）を取得する。
+
+    Parameters
+    ----------
+    date : str
+        検索する日付。形式は 'YYYY-MM-DD' です。
+
+    Returns
+    -------
+    dict
+        日ごとの支出総額を含む辞書。
+
+    """
+    # データの読み込み
+    if not file_path_obj.exists():
+        return {} # データ未追加
+    with open(file_path, 'r') as json_open:
+        j = json.load(json_open)
+    result_dict = defaultdict(int)
+
+    # 引数のパース
+    search_date = datetime.strptime(date, '%Y-%m-%d').date()
+    year = search_date.year
+    month = search_date.month
+
+    # 指定月の日数だけループ
+    for day in range(1, _get_days_of_month(year, month) + 1):
+        # 日ごとの支出合計を計算
+        day_amount = 0
+        for entry in j:
+            entry_date = datetime.strptime(entry["date"], '%Y-%m-%d').date()
+            if entry_date.year == year and entry_date.month == month and entry_date.day == day:
+                day_amount += entry["amount"]
+        # 日ごとの支出合計を辞書に追加
+        result_dict[day] = day_amount
+    return dict(result_dict)
+
+def _get_days_of_month(year, month):
+    """
+    指定された年月の日数を取得します。
+
+    Parameters
+    ----------
+    year : int
+        指定する年。
+    month : int
+        指定する月。
+
+    Returns
+    -------
+    int
+        指定された年月の日数。
+
+    """
+    # 月ごとの日数を格納したリスト
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if month == 2: # うるう年
+        if year % 4 == 0 and (year % 100 != 0 or y % 400 == 0):
+            return 29
+    else: # それ以外
+        return days[month - 1]
+
+def get_day_detail_data(date):
+    """
+    指定日の支出データをすべて取得する。
+
+    Parameters
+    ----------
+    date : str
+        検索する日付。形式は 'YYYY-MM-DD' です。
+
+    Returns
+    -------
+    dict
+        指定日の支出データを含む辞書。
+
+    """
+    if not file_path_obj.exists():
+        return {} # データ未追加
+    with open(file_path, 'r') as json_open:
+        j = json.load(json_open)
+    result_dict = defaultdict(int)
+
+    # 引数の変換
+    search_date = datetime.strptime(date, '%Y-%m-%d').date()
+    print(search_date)
+
+    count = 0
+    for entry in j:
+        entry_date = datetime.strptime(entry["date"], '%Y-%m-%d').date()
+        if entry_date.year == search_date.year and entry_date.month == search_date.month and entry_date.day == search_date.day:
+            result_dict[count] = entry
+            count += 1
+    return result_dict
+
 def get_graph_data(date):
     """
     指定された日付の各カテゴリーに対する支出データを取得します。
